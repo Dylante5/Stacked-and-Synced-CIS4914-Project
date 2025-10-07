1c7fa6940a581c228b4745519c22469a2cfe8119
WhosFish <167047218+WhosFish@users.noreply.github.com>
2025-10-06 22:32:28 -0400

    Delete apps/web directory

--- apps/web/app.js
+++
@@ -1,7 +0,0 @@
-// 当用户在编辑器里输入时，在终端输出
-const editor = document.getElementById("editor");
-const terminal = document.getElementById("terminal-output");
-
-editor.addEventListener("input", () => {
-  terminal.textContent = "> You typed: " + editor.value.split("\n").pop();
-});
