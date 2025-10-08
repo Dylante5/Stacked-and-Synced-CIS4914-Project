4db7e3cfa9420345b9d10df2718fde6d299dbafc
ReaperXYZ <166727639+ReaperXYZ@users.noreply.github.com>
2025-10-08 01:44:59 -0400

    Made it so code can be executed from terminal

--- codelink/web/src/pages/EditorPage.jsx
+++ codelink/web/src/pages/EditorPage.jsx
@@ -8,7 +8,7 @@ export default function EditorPage() {
       <div className="flex flex-col border rounded-lg overflow-hidden">
               <div className="p-2 font-semibold app-header mx-5">Editor
                   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-700 mx-91"
-                      onClick={() => console.log(monaco.editor.getEditors()[0].getValue())}>
+                      onClick={() => eval(monaco.editor.getEditors()[0].getValue())}>
                       Run
                   </button>
               </div>
