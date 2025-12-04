bceae67a6742d002c9e93cfd211d4634d41db800
mark-burnette <burnette.mark@ufl.edu>
2025-12-03 20:24:47 -0500

    fixed Upload function

--- codelink/web/src/functions/EditorFunctions.jsx
+++ codelink/web/src/functions/EditorFunctions.jsx
@@ -1,24 +1,26 @@
-import * as monaco from "monaco-editor";
 export function Download(text, name) {
+    // download file as JS
     var blob = new Blob([text], { type: 'text/plain' });
     var link = document.createElement('a');
+
     link.href = URL.createObjectURL(blob);
-    link.download = name + ".js";
+    link.download = name + ".js"; // add future language support
     link.click();
 }
 
-export function Upload() {
+export function Upload(editor) {
     var input = document.createElement('input');
     input.type = 'file';
 
     input.onchange = e => {
+        // read file content to editor
         var file = e.target.files[0];
         var reader = new FileReader();
 
         reader.readAsText(file, 'UTF-8');
         reader.onload = readerEvent => {
             var content = readerEvent.target.result;
-            monaco.editor.getEditors()[0].setValue(content);
+            editor.setValue(content);
         }
     }
     input.click();
